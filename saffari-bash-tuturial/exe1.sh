a5d243252642d276107b0c03a1faf6156e941b5f
moshe.kagan <moshe.kagan@innovid.com>
2016-12-25 20:01:13 +0200

    start the saffari first lesson

---
+++ saffari-bash-tuturial/exe1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp /tmp/messages /tmp/messages.old
+cat /dev/null > /tmp/messages
+
+echo log file copied and cleaned up
+
+exit 0
---
+++ saffari-bash-tuturial/my-first-bash-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#this is some commend
+echo hello worl
+echo 'hello planet mars'
