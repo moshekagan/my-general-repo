c5a7698f7e270222867b04dc7a437b37fb96882f
moshekagan <kaganmoshe90@gmail.com>
2016-12-27 12:43:57 +0200

    add new script to run mysql from remote server in terminal

--- my-scripts/run-mysql-quertys-from-remote-server-with-bash/RunQueryOnRemoteServer.sh
+++ my-scripts/run-mysql-quertys-from-remote-server-with-bash/RunQueryOnRemoteServer.sh
@@ -4,20 +4,21 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>> START >>>>>>>>>>>>>>>>>>>>>>"
 openLibre=$1
 libre="libre"
 host=wrote-here.your-mysql-host.com
+db=your-db
 path="~/repos/my-general-repo/my-scripts/run-mysql-quertys-from-remote-server-with-bash/query-output.csv"
 
-ssh $host << HERE
+ssh "$host" << HERE
  sudo su root
- mysql innovid -e "{ WRITE HERE YOUR QUERY }
+ mysql "$db" -e "{ WRITE HERE YOUR QUERY }
                    INTO OUTFILE '/tmp/query-output.csv'
                    FIELDS TERMINATED BY ','
                    ENCLOSED BY ''
                    LINES TERMINATED BY '\n';"
 HERE
 
-scp $host:/tmp/query-output.csv $path
+scp "$host":/tmp/query-output.csv $path
 
-ssh $host << HERE
+ssh "$host" << HERE
  sudo su root
  rm /tmp/query-output.csv
 HERE
