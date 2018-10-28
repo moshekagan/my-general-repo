520eba3b4474cebac0ae37d430edb1cecfbc3666
moshekagan <kaganmoshe90@gmail.com>
2018-10-28 19:00:13 +0200

    a

--- git_assets/pre-push-runner.py
+++ git_assets/pre-push-runner.py
@@ -1,13 +1,15 @@
+import os
 import subprocess
 
 gcp_sdk_path='/Users/kaganmoshe/workspace/google-cloud-sdk'
 illist_path='/Users/kaganmoshe/repos/illist'
 
-cwd = illist_path + '/illistsite'
+tests_running_directory = illist_path + '/illistsite'
 run_tests_command_args = ['python', illist_path + '/illistsite/test_runner.py', gcp_sdk_path + '/platform/google_appengine', '--test-path=tests/unit_tests']
 
 print 'Running tests...'
-if subprocess.Popen(run_tests_command_args, cwd=cwd) == 0:
+os.chdir(tests_running_directory)
+if subprocess.call(run_tests_command_args) == 0:
     print 'SUCCSS: tests passed succssfully'
     exit(0)
 else:
