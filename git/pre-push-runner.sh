a37ca9e94766a7bdbec912a59df32b2d668a39bf
moshekagan <kaganmoshe90@gmail.com>
2018-10-28 17:49:31 +0200

    aa

--- git/pre-push-runner.sh
+++ git/pre-push-runner.sh
@@ -10,10 +10,11 @@ echo About to run: python $illist_path/illistsite/test_runner.py $gcp_sdk_path/p
 python $illist_path/illistsite/test_runner.py $gcp_sdk_path/platform/google_appengine --test-path=tests
 test_result=$?
 
+
 if [ "$test_result" = 0 ]; then
     printf -- '\033[32m SUCCESS: Tests passed successfully :) \033[0m\n';
     exit 0
 else
-    printf -- '\033[31m ERROR: Some tests failed :( \033[0m\n';  
+    printf -- '\033[31m ERROR: Some tests failed :( \033[0m\n';
     exit 1
 fi
