66e98d69fd0dbb7e5e47c6709492c5d8b7f5181b
moshekagan <kaganmoshe90@gmail.com>
2018-10-28 18:50:08 +0200

    a

--- git_assets/pre-push-runner.py
+++ git_assets/pre-push-runner.py
@@ -6,7 +6,7 @@ illist_path='/Users/kaganmoshe/repos/illist'
 
 
 print 'Running tests...'
-if subprocess.call(['python', illist_path + '/illistsite/test_runner.py', gcp_sdk_path + '/platform/google_appengine', '--test-path=tests/unit_tests'], stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0:
+if subprocess.call(['python', illist_path + '/illistsite/test_runner.py', gcp_sdk_path + '/platform/google_appengine', '--test-path=tests/unit_tests']) == 0:
     print 'SUCCSS: tests passed succssfully'
     exit(0)
 else:
